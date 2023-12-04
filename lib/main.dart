1f59232bc12b24ddd4a5a3dd7c8ac8f9a9186f5d
shashank <shashankprince523@gmail.com>
2023-12-04 15:45:38 +0530

    v1 project basic implmentation done

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/generate_qr_code.dart';
+import 'package:qr_scanner/scan_qr_code.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "QR Scanner",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+  
+}
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar:AppBar(title: Text("QR scanner Page"),backgroundColor: Colors.blueAccent,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            ElevatedButton(onPressed: (){
+              setState(() {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ScanQRCode()));
+              });
+            }, child: Text("Scan Qr")),
+            const SizedBox(height: 40,),
+            ElevatedButton(onPressed:(){
+              setState(() {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>GenerateQRCode()));
+              });
+            },child: Text("Generate QR"))
+          ],
+        )
+
+      )
+
+    );
+  }
+}
+
